eec7ac355f1e4353069ed11c8440cbdc46707a31
samuelcend <mucasar24@gmail.com>
2025-02-17 22:32:37 -0500

    Update procedimientos.py

--- parser/procedimientos.py
+++ parser/procedimientos.py
@@ -10,7 +10,6 @@ def procesar_procedimiento(archivo, detallado=False):
     lineas_procedimiento = []
     dentro_del_procedimiento = False
     corchetes_abiertos = 0
-    encabezado = None
     nombre_proc = None  
 
     for linea in archivo:
@@ -18,9 +17,7 @@ def procesar_procedimiento(archivo, detallado=False):
 
         if linea.startswith("proc"):
             dentro_del_procedimiento = True
-            encabezado = linea  
-            if detallado:
-                print(f"Encabezado detectado -> '{encabezado}'")  
+            encabezado = linea.replace("proc", "", 1).strip()
             continue  
 
         if dentro_del_procedimiento:
@@ -32,23 +29,15 @@ def procesar_procedimiento(archivo, detallado=False):
         if dentro_del_procedimiento and corchetes_abiertos == 0:
             dentro_del_procedimiento = False  
 
-            if detallado:
-                print(f"Encabezado antes de procesar -> '{encabezado}'")
-
-            encabezado = encabezado.replace("proc", "", 1).strip()
             if ":" in encabezado:
                 nombre_proc, parametros = encabezado.split(":", 1)
                 nombre_proc = nombre_proc.strip()
                 parametros = parametros.strip().split()
             else:
-                if detallado:
-                    print(f"Procedimiento sin parámetros detectado -> '{encabezado}'")
                 nombre_proc = encabezado.replace("[", "").strip()
                 parametros = []  
 
             if not nombre_proc.isidentifier():
-                if detallado:
-                    print(f"Error: Nombre de procedimiento inválido '{nombre_proc}'")
                 return False
 
             procedimientos_definidos[nombre_proc] = {
